3235fba3568849378bfc7474f69423d01036f74c
reynardamadeus <reynard.starlee@gmail.com>
2026-08-10 09:51:50 +0700

    Initial Commit

---
+++ ARsemble/ARsembleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARsembleApp.swift
+//  ARsemble
+//
+//  Created by Reynard Amadeus  on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ARsembleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARsemble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARsemble
+//
+//  Created by Reynard Amadeus  on 10/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
